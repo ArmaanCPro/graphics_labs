61e8a9cc5003176e45289fe1db0dba7c7eacc9fa
Armaan Chahal <armaan.c2009@gmail.com>
2026-04-26 16:38:42 -0500

    Add testing with Doctest
    - Can disable with ENABLE_TESTS CMake option
    - Only testing UUIDv7 for now

---
+++ tests/EngerTests/src/TestUuid.cpp
@@ -0,0 +1,31 @@
+#include <doctest/doctest.h>
+#include "Utils/Uuid.h"
+
+TEST_CASE("uuidv7 basic validation")
+{
+    using namespace enger::uuid;
+    uuidv7 u1;
+    const auto data = u1.data();
+
+    SUBCASE("Version and Variant Bits")
+    {
+        // Version 7: Byte 6 high bits are all 0b0111 (0x70)
+        CHECK_EQ(data[6] & 0xF0, 0x70);
+        // Variant 2: Byte 8 high bits are 0b0010 (0x2)
+        CHECK_EQ((data[8] & 0xC0), 0x80);
+    }
+
+    SUBCASE("Monotonicity")
+    {
+        uuidv7 u2;
+        // Even if in the same millisecond, random bits are different
+        CHECK_LT(u1, u2);
+    }
+
+    SUBCASE("String Round-Trip")
+    {
+        std::string s = u1.to_string();
+        uuidv7 u3 = uuidv7::from_string(s);
+        CHECK_EQ(u1, u3);
+    }
+}
